2e40f0fccf887914b57b129d184b47d5e94f56c0
dewluffy <dewza20000@hotmail.com>
2025-07-22 16:11:01 +0700

    create route expenese location splits

---
+++ src/routes/splits.route.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const splitsRouter = express.Router({ mergeParams: true })
+
+// Get splits for an expense
+splitsRouter.get('/', (req, res) => {
+  res.send(`Get splits for expense ID: ${req.params.id}`)
+})
+
+// Create new splits
+splitsRouter.post('/', (req, res) => {
+  res.send(`Create splits for expense ID: ${req.params.id}`)
+})
+
+// Update a specific split (optional)
+splitsRouter.patch('/id', (req, res) => {
+  res.send(`Update split ID: ${req.params.splitId} in expense ID: ${req.params.id}`)
+})
+
+export default splitsRouter
