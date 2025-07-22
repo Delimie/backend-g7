909e5079004eb153e03a6df94423e7456e31a675
Foxfce <fenixz-1@hotmail.com>
2025-07-22 10:48:53 +0700

    Add folder shared/constants , Add socket.constant.js storing socket event for front and back

---
+++ src/shared/constants/socket.constant.js
@@ -0,0 +1,15 @@
+// Use for announce status
+export const CHAT_EVENT = {
+  NEW_MESSAGE: 'newMessage',
+  SYNC_MESSAGE: 'syncMessage',
+  CHAT_STATUS: 'chatStatus',
+  CHAT_ERROR: 'chatError',
+}
+
+// Use for emit action
+export const CHAT_ACTION = {
+  SEND_MESSAGE: 'sendMessage',
+  JOIN_CHAT: 'joinChat',
+  LEAVE_CHAT: 'leaveChat',
+  SYNC_MESSAGES: 'syncMessages',
+}
