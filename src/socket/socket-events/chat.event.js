6a9ec6505b4c3acbec0788dea40fce44ce8294f3
Foxfce <fenixz-1@hotmail.com>
2025-07-25 17:46:06 +0700

    add socket.on uesrtyping event, refactor socket folder structure separate handlers and event

--- src/socket/socket-events/chat.event.js
+++ src/socket/socket-events/chat.event.js
@@ -9,10 +9,14 @@ export const userTyping = (io, socket, data, callback) => {
       // console.log(`User : ${socket.user.name} is typing`);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
       socket.to('CHANNELID').emit(CHAT_ACTION.CHAT_TYPING, {
 =======
       socket.to('ROOMID').emit(CHAT_ACTION.CHAT_TYPING, {
 >>>>>>> 07338de (add socket.on uesrtyping event, refactor socket folder structure separate handlers and event)
+=======
+      socket.to('ROOMID').emit(CHAT_ACTION.CHAT_TYPING, {
+>>>>>>> 4670d99 (add socket.on uesrtyping event, refactor socket folder structure separate handlers and event)
         userName: socket.user.name,
         status: status
       });
@@ -20,10 +24,14 @@ export const userTyping = (io, socket, data, callback) => {
 
     // console.log(`User : ${socket.user.name} is not typing`);
 <<<<<<< HEAD
+<<<<<<< HEAD
     socket.to('CHANNELID').emit(CHAT_ACTION.CHAT_TYPING, {
 =======
     socket.to('ROOMID').emit(CHAT_ACTION.CHAT_TYPING, {
 >>>>>>> 07338de (add socket.on uesrtyping event, refactor socket folder structure separate handlers and event)
+=======
+    socket.to('ROOMID').emit(CHAT_ACTION.CHAT_TYPING, {
+>>>>>>> 4670d99 (add socket.on uesrtyping event, refactor socket folder structure separate handlers and event)
       userName: socket.user.name,
       status: status
     });
