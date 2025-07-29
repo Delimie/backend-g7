02534ebe40f003bca66715d82eb85ce9ef8a24a6
Foxfce <fenixz-1@hotmail.com>
2025-07-29 10:14:12 +0700

    Clean up git

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ import { Server } from 'socket.io';
 import { createServer } from 'http';
 import app from "./app.js";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import registerSocketRoute from './src/socket/webSocket.js';
 =======
 import registerSocketRoute from './webSocket.js';
 >>>>>>> 1a3f9c9 (add socket disconnection event)
+=======
+import registerSocketRoute from './src/socket/webSocket.js';
+>>>>>>> 6962f8e (Clean up git)
 
 dotenv.config()
 
